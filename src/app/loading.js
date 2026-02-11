eae12f0fe3c804ce2af0a1bf9bfe3cee876f68a8
Muhsin Azmal <muhsinstudent12@gmail.com>
2026-02-12 04:32:33 +0600

    feat: add global loading spinner component

---
+++ src/app/loading.js
@@ -0,0 +1,10 @@
+export default function Loading() {
+    return (
+        <div className="flex items-center justify-center min-h-[60vh]">
+            <div className="relative w-12 h-12">
+                <div className="absolute top-0 left-0 w-full h-full border-4 border-stone-200 rounded-full"></div>
+                <div className="absolute top-0 left-0 w-full h-full border-4 border-teal-900 rounded-full animate-spin border-t-transparent"></div>
+            </div>
+        </div>
+    );
+}
